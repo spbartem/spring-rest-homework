f30ca0970e3c42414283c9452f82044c491a3236
spbartem <spbartemm@gmail.com>
2024-06-28 11:51:26 +0300

    first commit

---
+++ src/main/java/product/star/spring/rest/HomeworkSpringRestApplication.java
@@ -0,0 +1,13 @@
+package product.star.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSpringRestApplication.class, args);
+	}
+
+}
